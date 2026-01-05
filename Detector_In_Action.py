0da9f518b0f4750f48b25f6a81025fa1e4631a4f
yous6sef <110791674+yous6sef@users.noreply.github.com>
2026-01-05 14:34:42 +0200

    Update model file name in Detector_In_Action.py

--- Detector_In_Action.py
+++ Detector_In_Action.py
@@ -9,7 +9,7 @@ from tensorflow.keras.layers import Dense,Conv2D,Flatten,MaxPooling2D,Dropout
 
 
 
-model = tf.keras.models.load_model('emotion_detection_model_gsn.h5')
+model = tf.keras.models.load_model('emotion_detection_model (1).h5')
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
 
